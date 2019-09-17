fb20c32abc86af692e0bdd7c70a4bfd1bcf079ed
Chasles <henry.chasles@escen.fr>
2019-09-17 15:21:57 +0200

    mardi 15h15

--- sw.js
+++ sw.js
@@ -1,32 +1,68 @@
-let fichier_offline /*ou cache_fichier*/ = [
-    "/index.html",
-    "/style.css",
-    "/reset.css",
-    "/app.js",
-]
-
-/* installer le worker*/
-
-self.addEventListener("install",function(event){
-    console.log("SW version - Install Event")
-    self.skipWaiting() /*permet de skip l'attente de l'installation*/
-    event.waitUntil( 
-
-        /*création d'un fichier cache*/
-
-        caches.open("cache-v1") 
-        .then(function(cache) {
-            console.log("Mise en cache des fichiers")
-            return cache.addAll(fichier_offline)
-        })
-        .catch(function(erreur){
-            
-        }
+let fichiers_offline = [
+    '/index.html',
+    '/style.css',
+    '/reset.css',
+    '/app.js'
+  ]
+  
+  
+  self.addEventListener("install",function(event){
+    console.warn("[SW] - Install Event")
+    self.skipWaiting()
+    event.waitUntil(
+      caches.open("cache-v1")
+      .then(function(cache) {
+                 console.log("Mise en cache des fichiers")
+                 return cache.addAll(fichiers_offline)
+      })
+      .catch(function(erreur){
+        console.log(erreur)
+      })
     )
-})
-/* activer le worker */
-
-self.addEventListener("activate",function(event){
-    console.log("SW version - activate Event")
-})
-/* installer le worker*/    
+  })
+  
+  self.addEventListener("activate",function(event){
+    console.warn("[SW] - Activate Event")
+  })
+  
+  self.addEventListener("fetch",function(event){
+    console.warn("[SW] - Fetch Event")
+    console.log(event.request);
+    // if(event.request.url == "http://localhost:5000/page2") {
+    //   event.respondWith(
+    //     new Response("Pas de chance... requête interceptée et modifée...")
+    //   )
+    // }
+  
+    event.respondWith(
+      fromCache(event.request)
+      .then(function(response) {
+             console.warn("Il y a bien correspondance dans le cache "+event.request.url);
+             return response;
+      })
+      .catch(function(response) {
+             console.warn("Pas de fichier en cache... On va chercher en ligne sur le serveur avec fetch "+event.request.url);
+             return fetch(event.request)
+      })
+    )
+  })
+  
+  
+  /* Récupérer les fichiers en cache */
+  /* Fonctions Gestion Cache */
+  
+  function fromCache(request) {
+    // Vérifier si le fichier est en cache (cache.match)
+    // Retourne la réponse
+    // Sinon reject...
+    return caches.open("cache-v1").then(function(cache) {
+      return cache.match(request).then(function(matching) {
+                if (!matching || matching.status === 404) {
+                  return Promise.reject("no-match");
+                }
+                else {
+                  return matching;
+                }
+      });
+    });
+  } 
